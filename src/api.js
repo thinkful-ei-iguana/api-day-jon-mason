01c9b9a5ceb1ef10cb58f9cb7e9dd378f9cf0bb4
Mason Reichbauer <mreichba@mail.usf.edu>
2019-11-04 15:30:47 -0500

    switching at 6

--- src/api.js
+++ src/api.js
@@ -1,9 +1,24 @@
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[yourname]';
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/mason';
+
+function createItem(name){
+  let newItem = JSON.stringify({
+    name, 
+  });
+  return fetch(`${BASE_URL}/items`, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: newItem
+  });
+}
 
 const getItems = function () {
-  return Promise.resolve('A successful response!');
+  return fetch (`${BASE_URL}/items`)
+    .then(response => response.json());
+  
+  // return Promise.resolve('A successful response!');
 };
 
 export default {
-  getItems
+  getItems,
+  createItem
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import $ from 'jquery';
-
+import api from './api';
+import store from './store.js';
 import 'normalize.css';
 import './index.css';
 
 import shoppingList from './shopping-list';
 
 const main = function () {
+  api.getItems()
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
